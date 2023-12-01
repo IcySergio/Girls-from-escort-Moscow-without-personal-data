edf0f3e911d4f33acb1a4cdc7be0dc7bf6feba94
IcySergio <106556861+IcySergio@users.noreply.github.com>
2023-12-01 19:57:57 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
 import plotly.express as plt
 import csv
 st.title('Girls from escort (Moscow) without personal data')
